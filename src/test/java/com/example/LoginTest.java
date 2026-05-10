24feabd3a13cbec4cfa76e3748a26d4f29b50fb5
AnuragPatil3781 <anuragpatil3781@gmail.com>
2026-05-11 01:08:57 +0530

    Initial Selenium Maven project

---
+++ src/test/java/com/example/LoginTest.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+
+    @Test
+    public void loginTest() throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.saucedemo.com/");
+
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+
+        driver.findElement(By.id("login-button")).click();
+
+        System.out.println("Login Successful");
+
+        Thread.sleep(3000);
+
+        driver.quit();
+    }
+}
